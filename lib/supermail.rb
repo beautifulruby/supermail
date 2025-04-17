b0cc690e27d9a4775411349e41bd3ccb0f789cbf
Brad Gessler <bradgessler@gmail.com>
2025-04-16 22:04:02 -0700

    Initial commit

---
+++ lib/supermail.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "supermail/version"
+
+module Supermail
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/supermail_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Supermail do
+  it "has a version number" do
+    expect(Supermail::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
