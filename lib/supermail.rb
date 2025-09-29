d15737b12f5247e5f0203a166cdae8fa7174c0c8
Brad Gessler <bradgessler@gmail.com>
2025-09-29 01:07:59 -0700

    Add Mailto link generation

--- lib/supermail.rb
+++ lib/supermail.rb
@@ -2,6 +2,7 @@
 
 require_relative "supermail/version"
 require "action_mailer"
+require "erb"
 
 module Supermail
   class Error < StandardError; end
@@ -18,11 +19,40 @@ module Supermail
       def to = nil
       def from = nil
       def subject = nil
+      def cc = []
+      def bcc = []
       def body = ""
 
+      # Generate a mailto: URL with appropriate escaping.
+      def mailto = MailTo.href(to:, from:, cc:, bcc:, subject:, body:)
+      alias :mail_to :mailto
+
       private def action_mailer_base_mail
-        ActionMailer::Base.mail(to:, from:, subject:, body:)
+        ActionMailer::Base.mail(to:, from:, cc:, bcc:, subject:, body:)
       end
+    end
+  end
+
+  module MailTo
+    extend self
+
+    def href(to:, **params)
+      q = query(**params)
+      q.empty? ? "mailto:#{to}" : "mailto:#{to}?#{q}"
+    end
+
+    def query(**params)
+      params
+        .compact                          # drop nils
+        .reject { |k, v| v.is_a?(Array) && v.empty? }  # drop empty arrays
+        .map { |k, v| "#{k}=#{mailto_escape(v)}" }
+        .join("&")
+    end
+
+    private
+
+    def mailto_escape(str)
+      ERB::Util.url_encode(str.to_s).tr("+", "%20")
     end
   end
 end
